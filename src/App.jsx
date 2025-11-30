04643fc8ecc9c39042644a851e8445c2e24f6884
Angel Romero <angel.romero.ssid@gmail.com>
2025-11-29 19:33:02 -0600

    Score System initialv5

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ import { fallbackTeams } from "./fallbackTeams";
 export default function App() {
   return (
     <FullscreenRoboRAVE>
-      <h2 style={{ marginBottom: "20px", color: "white" }}>
-        Live Score
-      </h2>
+      
 
       {/* Aquí se ve tu versión PREMIUM por categorías */}
       <LiveHeatsByCategory teams={fallbackTeams} />
