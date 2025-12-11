d696101262d37ee902f517f331340ccbfbdb9e19
Angel Romero <angel.romero.ssid@gmail.com>
2025-12-11 01:45:33 -0600

    NewFeatures3

--- src/fallbackTeams.js
+++ src/fallbackTeams.js
@@ -10,5 +10,15 @@ export const fallbackTeams = [
   { id: 8,  name: "ValleBot_4",                 score: 0, flag:"mx", logo:"vallebot.png",     category: "SUMO_ES" },
   { id: 9,  name: "ValleBot_3",                 score: 0, flag:"mx", logo:"vallebot.png",     category: "SUMO_ES" },
   { id: 10, name: "Iron Bots",                  score: 0, flag:"mx", logo:"logomx.png",       category: "SUMO_ES" },
-
+  { id: 11, name: "Mechanic Fight",             score: 0, flag:"mx", logo:"logomx.png",       category: "A-MAZE-ING_ES" },
+  { id: 12, name: "Short Circuit",              score: 0, flag:"mx", logo:"logomx.png",       category: "A-MAZE-ING_ES" },
+  { id: 13, name: "Conalep Piedras Negras",     score: 0, flag:"mx", logo:"conaleppn.png",    category: "A-MAZE-ING_ES" },
+  { id: 14, name: "Innademy - Elian",           score: 0, flag:"mx", logo:"innademy.png",     category: "A-MAZE-ING_ES" },
+  { id: 15, name: "Innademy - Santino",         score: 0, flag:"mx", logo:"innademy.png",     category: "A-MAZE-ING_ES" },
+  { id: 16, name: "Innademy - Annia",           score: 0, flag:"mx", logo:"innademy.png",     category: "A-MAZE-ING_ES" },
+  { id: 17, name: "FireFighters Paxtle",        score: 0, flag:"mx", logo:"paxtle.png",       category: "A-MAZE-ING_ES" },
+  { id: 18, name: "BERSERK",                    score: 0, flag:"mx", logo:"conalepvs.png",    category: "A-MAZE-ING_ES" },
+  { id: 19, name: "GOWAN",                      score: 0, flag:"mx", logo:"conalepvs.png",    category: "A-MAZE-ING_ES" },
+  { id: 20, name: "LA GOTA GORDA",              score: 0, flag:"mx", logo:"kidstronic.png",   category: "A-MAZE-ING_ES" },
+  
 ];
